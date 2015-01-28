39e946c0ce7b60c2d839226f8292e219b700fb3c
Hao <haoliu350@gmail.com>
2015-01-28 10:00:17 -0500

    initial commit

---
+++ fitch-filter-module/src/main/java/com/fitch/filter/FilterModule.java
@@ -0,0 +1,12 @@
+package com.fitch.filter;
+
+/**
+ * This class is optional and represents the configuration for the fitch-filter-module module.
+ * By exposing simple getter/setter/adder methods, this bean can be configured via content2bean
+ * using the properties and node from <tt>config:/modules/fitch-filter-module</tt>.
+ * If you don't need this, simply remove the reference to this class in the module descriptor xml.
+ */
+public class FilterModule {
+    /* you can optionally implement info.magnolia.module.ModuleLifecycle */
+
+}
